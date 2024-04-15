03c49ac23a6acc232ecce332be73e2f39077d4aa
Davr <yul.davron.93@gmail.com>
2024-04-15 20:44:03 +0500

    parser is ready

--- src/index.ts
+++ src/index.ts
@@ -64,64 +64,92 @@ const main = async () => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         const orderId = match[0];
         console.log('row', row);
+        const logs = await new Promise((resolve, reject) => {
+          ajax.postJson('/Orders/GetLog', null, { orderUUId: orderId }, (data) => {
+            console.log('data', data)
+            resolve(data.logs);
+          }, (error) => {
+            reject(error);
+          })
+        });
 
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-        const orderFromCell = row.querySelector(".b-table__col:nth-child(7)");
-        console.log('orderFromCell', orderFromCell);
+        // console.log('logs', logs)
+        // await new Promise((resolve) => { setTimeout(resolve, 50000) });
+        const acceptedLog = logs.find(log => log.Message.indexOf('has been accepted') !== -1);
+        if (acceptedLog) {
 
-        if (orderFromCell) {
           // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-          const orderFrom = orderFromCell.querySelector("span:first-child");
-          console.log('orderFrom', orderFrom);
-          if (orderFrom) {
+          const orderFromCell = row.querySelector(".b-table__col:nth-child(7)");
+          console.log('orderFromCell', orderFromCell);
+
+          if (orderFromCell) {
             // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-            const orderFromText = orderFrom.textContent?.trim();
-            console.log('orderFromText', orderFromText);
-            if (orderFromText) {
-              const regex = /^(\D+)\s\d+$/;
+            const orderFrom = orderFromCell.querySelector("span:first-child");
+            console.log('orderFrom', orderFrom);
+            if (orderFrom) {
               // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-              const orderFromTextMatch = orderFromText.match(regex);
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-              if (orderFromTextMatch) {
+              const orderFromText = orderFrom.textContent?.trim();
+              console.log('orderFromText', orderFromText);
+              if (orderFromText) {
+                const regex = /^(\D+)\s\d+$/;
                 // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-                const orderFromTextMatch2 = orderFromTextMatch[1];
+                const orderFromTextMatch = orderFromText.match(regex);
                 // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-                switch (orderFromTextMatch2?.toLocaleLowerCase()) {
-                  case "узум":
-                    return {
-                      orderId,
-                      order_from: 'uzum'
-                    };
-                  case "exp":
-                    return {
-                      orderId,
-                      order_from: 'express'
-                    };
-                  case "yan":
-                    return {
-                      orderId,
-                      order_from: 'yandex'
-                    };
-                  default:
-                    console.log("Unknown order from", orderFromTextMatch2);
-                    return {
-                      orderId
-                    };
+                if (orderFromTextMatch) {
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+                  const orderFromTextMatch2 = orderFromTextMatch[1];
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+                  switch (orderFromTextMatch2?.toLocaleLowerCase()) {
+                    case "узум":
+                      return {
+                        date: acceptedLog.EventDateTime,
+                        user: acceptedLog.EmployeeName,
+                        orderId,
+                        order_from: 'uzum'
+                      };
+                    case "exp":
+                      return {
+                        date: acceptedLog.EventDateTime,
+                        user: acceptedLog.EmployeeName,
+                        orderId,
+                        order_from: 'express'
+                      };
+                    case "yan":
+                      return {
+                        date: acceptedLog.EventDateTime,
+                        user: acceptedLog.EmployeeName,
+                        orderId,
+                        order_from: 'yandex'
+                      };
+                    default:
+                      console.log("Unknown order from", orderFromTextMatch2);
+                      return {
+                        date: acceptedLog.EventDateTime,
+                        user: acceptedLog.EmployeeName,
+                        orderId,
+                        order_from: 'others'
+                      };
+                  }
+                } else {
+                  console.log("No match found");
+                  return {
+                    date: acceptedLog.EventDateTime,
+                    user: acceptedLog.EmployeeName,
+                    orderId,
+                    order_from: 'others'
+                  };
                 }
               } else {
-                console.log("No match found");
                 return {
-                  orderId
-                };
-              }
-            } else {
-              return {
-                orderId
+                  date: acceptedLog.EventDateTime,
+                  user: acceptedLog.EmployeeName,
+                  orderId,
+                  order_from: 'others'
+                }
               }
             }
           }
         }
-
         return {
           orderId,
         };
@@ -138,32 +166,32 @@ const main = async () => {
 
   // console.log('result', result)
 
-  const orders = result.filter(item => { return item.order_from !== undefined });
+  const orders = result.filter(item => { return item.order_from !== undefined && item.user && item.user.length > 0 });
 
-  console.log('orders', orders)
+  // console.log('orders', orders)
 
-  const ordersData = await page.evaluate(async (orders) => {
-    let result = {};
+  // const ordersData = await page.evaluate(async (orders) => {
+  //   let result = {};
 
-    for await (const order of orders) {
-      result[order.orderId] = await new Promise((resolve, reject) => {
-        ajax.postJson('/Orders/GetLog', null, { orderUUId: order.orderId }, (data) => {
-          console.log('data', data)
-          resolve({
-            logs: data.logs,
-            from: order.order_from
-          });
-        }, (error) => {
-          reject(error);
-        })
-      });
+  //   for await (const order of orders) {
+  //     result[order.orderId] = await new Promise((resolve, reject) => {
+  //       ajax.postJson('/Orders/GetLog', null, { orderUUId: order.orderId }, (data) => {
+  //         console.log('data', data)
+  //         resolve({
+  //           logs: data.logs,
+  //           from: order.order_from
+  //         });
+  //       }, (error) => {
+  //         reject(error);
+  //       })
+  //     });
 
-    }
+  //   }
 
-    return result;
-  }, orders)
+  //   return result;
+  // }, orders)
 
-  console.log('ordersData', ordersData)
+  console.log('ordersData', orders)
   await new Promise((resolve) => { setTimeout(resolve, 10000) });
 };
 
