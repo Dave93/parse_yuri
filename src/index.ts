56e592fa111b05f4c1eb21f5a4c3f4b7f7cbfe04
Davr <yul.davron.93@gmail.com>
2024-04-15 20:44:48 +0500

    parser is ready

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ dotenv.config();
 const main = async () => {
 
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
